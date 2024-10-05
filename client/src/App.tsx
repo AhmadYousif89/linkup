d12f8c4e4716cb93aa786c30cf8a0696b126e06c
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2024-10-06 01:57:06 +0300

    Add pages home, dashboard and create main app router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
-import Home from './pages/home/home';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/home/home";
+import Dashboard from "./pages/dashboard/dashboard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<Home />}>
+      <Route path="home" element={<Home />} />
+    </Route>,
+    <Route path="dashboard" element={<Dashboard />} />,
+  ]),
+);
 
 export default function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
---
+++ client/src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import { MainHeader } from "./header";
+import { HeroSection } from "./hero";
+
+export default function Home() {
+  return (
+    <main className="">
+      <MainHeader />
+      <HeroSection />
+    </main>
+  );
+}
